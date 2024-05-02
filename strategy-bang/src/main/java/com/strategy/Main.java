12513d85c455a2d11f2d2f61a0a4a7e1a7f4bc4f
yjj <lejianjun@shizhuang-inc.com>
2024-05-02 20:36:15 +0800

    Initial commit

---
+++ strategy-bang/src/main/java/com/strategy/Main.java
@@ -0,0 +1,12 @@
+package com.strategy;
+
+/**
+ * @author
+ * @date ${DATE}
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
